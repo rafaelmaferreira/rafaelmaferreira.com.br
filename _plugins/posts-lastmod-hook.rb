b05f8138e1cac785b0e309121cbef1daddd3932b
rafaelmaferreira <rafael.low1@gmail.com>
2024-05-18 18:50:28 -0300

    teste3

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -4,10 +4,12 @@
 
 Jekyll::Hooks.register :posts, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  # Get the number of commits for the post
+  commit_num = `git rev-list --count HEAD "#{post.path}"`.to_i
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+  # If the post has more than one commit, update the last modified date
+  if commit_num > 1
+    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{post.path}"`.strip
     post.data['last_modified_at'] = lastmod_date
   end
 
